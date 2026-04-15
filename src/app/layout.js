4402be1f352dda3079ba9e538341f13e951734b2
Md. Amirul Islam <amirulislam.office28@gmail.com>
2026-04-15 19:40:01 +0600

    chore: import friends data in json format

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "./Components/NavBar/page";
+import Footer from "./Components/Footer/page";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,7 +26,9 @@ export default function RootLayout({ children }) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
+        <Navbar></Navbar>
         {children}
+        <Footer></Footer>
         </body>
     </html>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,9 @@
 import Banner from "./Components/Banner/page";
-import Footer from "./Components/Footer/page";
-import Navbar from "./Components/NavBar/page";
 
 export default function Home() {
   return (
     <>
-    <Navbar></Navbar>
     <Banner></Banner>
-    <Footer></Footer>
     </>
   );
 }
